2e7c3e38d244a06bf034bc02c821c007491c5fd7
sidit77 <sidit77@gmail.com>
2024-05-04 23:56:02 +0200

    support for simple enums

--- instructor-derive-internals/src/lib.rs
+++ instructor-derive-internals/src/lib.rs
@@ -1,6 +1,6 @@
-use proc_macro2::{TokenStream};
+use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote, ToTokens};
-use syn::{Attribute, Data, DeriveInput, Fields, LitStr, Result};
+use syn::{Attribute, Data, DataEnum, DataStruct, DeriveInput, Fields, LitStr, Result};
 
 #[derive(Debug)]
 enum Endian {
@@ -40,58 +40,132 @@ fn parse_top_level_attributes(attrs: &Vec<Attribute>) -> Result<Endian> {
     Ok(endian)
 }
 
+fn get_repr(attrs: &Vec<Attribute>) -> Result<Option<Ident>> {
+    let mut repr = None;
+    for attr in attrs {
+        if attr.path().is_ident("repr") {
+            attr.parse_nested_meta(|meta| {
+                repr = meta.path.get_ident().cloned();
+                Ok(())
+            })?;
+        }
+    }
+    Ok(repr)
+}
+
+fn is_default(attrs: &Vec<Attribute>) -> Result<bool> {
+    let mut default = false;
+    for attr in attrs {
+        if attr.path().is_ident("instructor") {
+            attr.parse_nested_meta(|meta| {
+                default |= meta.path.is_ident("default");
+                Ok(())
+            })?;
+        }
+    }
+    Ok(default)
+}
+
 pub fn derive_unpack(input: DeriveInput) -> Result<TokenStream> {
     let DeriveInput { ident, data, attrs, ..} = input;
 
     let endian = parse_top_level_attributes(&attrs)?;
 
-    let tokens = match data {
-        Data::Struct(data) => {
-            let mut fields = Vec::new();
-            let mut statements = Vec::new();
-            for field in data.fields.iter() {
-                let ident = field
-                    .ident
-                    .clone()
-                    .unwrap_or_else(|| format_ident!("field_{}", fields.len()));
-                let ty = &field.ty;
-                statements.push(quote! {
+
+    match data {
+        Data::Struct(data) => generate_struct_impl(endian, ident, data),
+        Data::Enum(data) => match get_repr(&attrs)? {
+            Some(repr) => generate_enum_impl(endian, repr, ident, data),
+            None => Err(syn::Error::new_spanned(ident, "enums must have a repr attribute")),
+        }
+        Data::Union(_) => Err(syn::Error::new_spanned(ident, "unions are not supported")),
+    }
+}
+
+fn generate_struct_impl(endian: Endian, ident: Ident, data: DataStruct) -> Result<TokenStream> {
+    let mut fields = Vec::new();
+    let mut statements = Vec::new();
+    for field in data.fields.iter() {
+        let ident = field
+            .ident
+            .clone()
+            .unwrap_or_else(|| format_ident!("field_{}", fields.len()));
+        let ty = &field.ty;
+        statements.push(quote! {
                     let #ident: #ty = instructor::Unpack::<#endian>::unpack(buffer)?;
                 });
-                fields.push(ident);
-            }
-            let ret = match data.fields {
-                Fields::Named(_) => quote! {
+        fields.push(ident);
+    }
+    let ret = match data.fields {
+        Fields::Named(_) => quote! {
                     Self {
                         #(#fields),*
                     }
                 },
-                Fields::Unnamed(_) => quote! {
+        Fields::Unnamed(_) => quote! {
                     Self (#(#fields),*)
                 },
-                Fields::Unit => quote! {
+        Fields::Unit => quote! {
                     Self
                 }
-            };
-            let generic = match endian {
-                Endian::Generic => quote! { <E: instructor::Endian> },
-                _ => quote! {},
-            };
-            quote! {
-                impl #generic instructor::Unpack<#endian> for #ident {
-                    fn unpack<B: instructor::Buffer + ?Sized>(buffer: &mut B) -> Result<Self, instructor::Error> {
-                        #(#statements)*
-                        Ok(#ret)
-                    }
+    };
+    let generic = match endian {
+        Endian::Generic => quote! { <E: instructor::Endian> },
+        _ => quote! {},
+    };
+    let output = quote! {
+        #[automatically_derived]
+        impl #generic instructor::Unpack<#endian> for #ident {
+            fn unpack<B: instructor::Buffer + ?Sized>(buffer: &mut B) -> Result<Self, instructor::Error> {
+                #(#statements)*
+                Ok(#ret)
+            }
+        }
+    };
+    Ok(output)
+}
+
+fn generate_enum_impl(endian: Endian, repr: Ident, ident: Ident, data: DataEnum) -> Result<TokenStream> {
+    let mut default = None;
+    let mut variants = Vec::new();
+    for variant in data.variants.iter() {
+        let ident = &variant.ident;
+        let discr = match &variant.discriminant {
+            Some((_, expr)) => expr,
+            None => return Err(syn::Error::new_spanned(ident, "every variant must have a discriminant")),
+        };
+        if is_default(&variant.attrs)? {
+            if default.is_some() {
+                return Err(syn::Error::new_spanned(ident, "only one variant can be marked as default"));
+            }
+            default = Some(ident.clone());
+        }
+        variants.push(quote! {
+            #discr => Ok(Self::#ident)
+        });
+    }
+    let generic = match endian {
+        Endian::Generic => quote! { <E: instructor::Endian> },
+        _ => quote! {},
+    };
+    let default = match default {
+        Some(ident) => quote! { _ => Ok(Self::#ident) },
+        None => quote! { _ => Err(instructor::Error::InvalidValue) },
+    };
+    let output = quote! {
+        #[automatically_derived]
+        impl #generic instructor::Unpack<#endian> for #ident {
+            fn unpack<B: instructor::Buffer + ?Sized>(buffer: &mut B) -> Result<Self, instructor::Error> {
+                let value: #repr = instructor::Unpack::<#endian>::unpack(buffer)?;
+                match value {
+                    #(#variants,)*
+                    #default,
                 }
             }
         }
-        _ => return Err(syn::Error::new_spanned(ident, "expected struct")),
     };
-
-    Ok(tokens)
+    Ok(output)
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -101,7 +175,29 @@ mod tests {
     fn print_deserialize() {
         let input = syn::parse_quote! {
             #[instructor(endian = "little")]
+            #[repr(align(4))]
             struct Header(u32);
+        };
+
+        //let input = syn::parse_quote! {
+        //    struct Header {
+        //        #[field]
+        //        sdf: u32,
+        //    }
+        //};
+        let output = derive_unpack(input).unwrap();
+        let formatted = prettyplease::unparse(&syn::parse2(output).unwrap());
+        print!("{}", formatted);
+    }
+
+    #[test]
+    fn print_enum() {
+        let input = syn::parse_quote! {
+            #[repr(u8)]
+            enum Data {
+                A = 0x01,
+                B = 0x02
+            }
         };
 
         //let input = syn::parse_quote! {
--- instructor/examples/derive.rs
+++ instructor/examples/derive.rs
@@ -5,6 +5,9 @@ fn main() {
     let mut data: &[u8] = &[0x06, 0x00, 0x01, 0x00, 0x0a, 0x02, 0x02, 0x00, 0x02, 0x00];
     let l2cap: L2capHeader = data.read().unwrap();
     println!("{:?}", l2cap);
+    let signaling: SignalingHeader = data.read().unwrap();
+    println!("{:?}", signaling);
+    println!("{:X?}", data);
 }
 
 
@@ -15,5 +18,35 @@ struct L2capHeader {
     cid: u16
 }
 
+#[derive(Debug, Unpack)]
+#[instructor(endian = "little")]
+struct SignalingHeader {
+    code: SignalingCodes,
+    id: u8,
+    length: Length<u16, 0>
+}
 
-
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Unpack)]
+#[repr(u8)]
+enum SignalingCodes {
+    CommandReject = 0x01,
+    ConnectionRequest = 0x02,
+    ConnectionResponse = 0x03,
+    ConfigureRequest = 0x04,
+    ConfigureResponse = 0x05,
+    DisconnectionRequest = 0x06,
+    DisconnectionResponse = 0x07,
+    EchoRequest = 0x08,
+    EchoResponse = 0x09,
+    InformationRequest = 0x0A,
+    InformationResponse = 0x0B,
+    ConnectionParameterUpdateRequest = 0x12,
+    ConnectionParameterUpdateResponse = 0x13,
+    LECreditBasedConnectionRequest = 0x14,
+    LECreditBasedConnectionResponse = 0x15,
+    FlowControlCreditIndex = 0x16,
+    CreditBasedConnectionRequest = 0x17,
+    CreditBasedConnectionResponse = 0x18,
+    CreditBasedReconfigurationRequest = 0x19,
+    CreditBasedReconfigurationResponse = 0x1A,
+}
